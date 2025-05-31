8fec7d9f74fed1d9b346c67d6b23a852532f2f2f
igrekus <upload234@mail.ru>
2025-05-31 15:07:11 +0300

    doc: add description

--- src/deb_resolve/args.py
+++ src/deb_resolve/args.py
@@ -1,10 +1,20 @@
+"""
+Resolve APT dependencies for given input
+
+Two major modes
+
+  - list packages to be added relative to the current APT cache state -- this is what APT does on 'apt install'
+  - recursively list all dependencies for given input up to [level]
+
+"""
 import argparse
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('apt packages', nargs='+',
-                        help='apt packages to resolve dependencies for')
+    parser.add_argument('packages', nargs='+',
+                        help='APT packages to resolve dependencies for')
     parser.add_argument('--all-deps', action='store_true',
                         help='recursively find all dependencies')
     parser.add_argument('--installed', action='store_true',
